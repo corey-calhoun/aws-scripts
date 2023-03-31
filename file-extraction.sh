091f36da4825c3fcef6a6c616cc334c9cd94bec4
Corey C <67290010+corey-calhoun@users.noreply.github.com>
2023-03-31 15:01:57 -0500

    Create file-extraction.sh
    Initial commit

---
+++ file-extraction.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+################################################################################
+# Author: Corey Calhoun
+# Date: 03-31-2023
+# Description: Filetype search and extraction script for AWS S3 buckets.
+# Usage: ./file-extraction.sh
+################################################################################
+
+# Ask the user for the S3 bucket name
+read -p "Enter the S3 bucket name: " bucket_name
+
+# Ask the user for the file type to search for
+read -p "Enter the file type to search for (without leading dot): " file_type
+
+# Ask the user for the destination directory to copy the files to
+read -p "Enter the destination directory: " destination_dir
+
+# Search for the files in the S3 bucket with the specified file type
+search_results=$(aws s3 ls s3://$bucket_name | grep ".$file_type")
+
+# Check if files were found
+if [[ -n $search_results ]]; then
+  # Loop through the search results
+  while read -r line; do
+    # Get the file name
+    file_name=$(echo $line | awk '{print $4}')
+
+    # Copy the file to the destination directory
+    aws s3 cp s3://$bucket_name/$file_name $destination_dir
+  done <<< "$search_results"
+else
+  echo "No files found."
+fi 
